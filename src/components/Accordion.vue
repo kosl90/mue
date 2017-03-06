814b83b3b817eb9d0ddb9193ff9a716bdf8ad35b
kosl90 <kos1990l@gmail.com>
2017-03-06 17:23:07 +0800

    accordion template

---
+++ src/components/Accordion.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class='v-accordion'>
+    <header class='v-accordion__header' role='heading' :aria-level='level'>
+      <button class='v-accordion__header-btn' role='button' :aria-disabled='panelVisible' :aria-controls='panelID'>
+        <slot name='header'></slot>
+      </button>
+    </header>
+    <section :id='panelID' :aria-visible='panelVisible'>
+      <slot name='panel'></slot>
+    </section>
+  </div>
+</template>
