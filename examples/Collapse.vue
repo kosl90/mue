95c20d801eb3ad3d7adaab4a43c3e811150543ec
kosl90 <kos1990l@gmail.com>
2017-03-07 22:15:15 +0800

    add a border to collapse section

--- examples/Collapse.vue
+++ examples/Collapse.vue
@@ -1,33 +1,47 @@
+<style scoped>
+  .example {
+    margin: 0 1em;
+    border: 2px lightgray solid;
+  }
+</style>
+
 <template>
   <section>
     <h2>Accordion Example</h2>
-    <h3>Generic Collapse</h3>
-    <VCollapse>
-      <VCollapseItem v-for='item in items'>
-        <template slot="title">{{ item.title }}</template>
-        <p>{{ item.content }}</p>
-      </VCollapseItem>
-    </VCollapse>
 
-    <h3>loose mode Accordion</h3>
-    <VCollapse accordion>
-      <VCollapseItem v-for='(item, i) in items' :active='i===0'>
-        <template slot="title">{{ item.title }}</template>
-        <p>{{ item.content }}</p>
-      </VCollapseItem>
-      <VCollapseItem>
-        <template slot="title">header 2</template>
-        <p>test2</p>
-      </VCollapseItem>
-    </VCollapse>
+    <section>
+      <h3>Generic Collapse</h3>
+      <VCollapse class='example'>
+        <VCollapseItem v-for='item in items'>
+          <template slot="title">{{ item.title }}</template>
+          <p>{{ item.content }}</p>
+        </VCollapseItem>
+      </VCollapse>
+    </section>
 
-    <h3>strict mode Accordion</h3>
-    <VCollapse accordion='strict'>
-      <VCollapseItem v-for='item in items'>
-        <template slot="title">{{ item.title }}</template>
-        <p>{{ item.content }}</p>
-      </VCollapseItem>
-    </VCollapse>
+    <section>
+      <h3>loose mode Accordion</h3>
+      <VCollapse accordion class='example'>
+        <VCollapseItem v-for='(item, i) in items' :active='i===0'>
+          <template slot="title">{{ item.title }}</template>
+          <p>{{ item.content }}</p>
+        </VCollapseItem>
+        <VCollapseItem>
+          <template slot="title">header 2</template>
+          <p>test2</p>
+        </VCollapseItem>
+      </VCollapse>
+    </section>
+
+    <section>
+      <h3>strict mode Accordion</h3>
+      <VCollapse accordion='strict' class='example'>
+        <VCollapseItem v-for='item in items'>
+          <template slot="title">{{ item.title }}</template>
+          <p>{{ item.content }}</p>
+        </VCollapseItem>
+      </VCollapse>
+    </section>
   </section>
 </template>
 
