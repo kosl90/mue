4b9b8631646dec333f4eaf0616d1e2dc69d96a78
kosl90 <kos1990l@gmail.com>
2017-03-08 23:04:35 +0800

    plan to use cssnext instead of sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 /* eslint-disable global-require, import/no-extraneous-dependencies */
 module.exports = {
-  parser: require('postcss-scss'),
+  // parser: require('postcss-scss'),
   plugins: [
     require('autoprefixer')({
       browsers: ['last 2 versions'],
     }),
-    require('precss'),
+    // require('precss'),
   ],
 };
